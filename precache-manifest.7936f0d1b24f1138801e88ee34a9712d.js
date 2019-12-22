c0268273290702cfc518d03167bbf87351018abf
ghousemohamed <ghousemohamed7126@gmail.com>
2019-12-23 00:12:09 +0530

    Updates

---
+++ precache-manifest.7936f0d1b24f1138801e88ee34a9712d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9846b8f9f9b82d441dc8bd79286f6aae",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "53e5a5c79608fdfeb3b5",
+    "url": "/github-finder/static/css/main.5d4df402.chunk.css"
+  },
+  {
+    "revision": "5f46883d43a673e15f80",
+    "url": "/github-finder/static/js/2.c5ec40bc.chunk.js"
+  },
+  {
+    "revision": "53e5a5c79608fdfeb3b5",
+    "url": "/github-finder/static/js/main.54698510.chunk.js"
+  },
+  {
+    "revision": "a06ceeb70725f86afdce",
+    "url": "/github-finder/static/js/runtime~main.cb74c248.js"
+  }
+]);
